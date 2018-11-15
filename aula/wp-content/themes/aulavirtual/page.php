5d69389146a86e905b7cd60047c5374bf2a5d072
yeison8921 <yeison8921@gmail.com>
2018-11-15 13:14:06 -0500

    Octavo adelanto música

--- aula/wp-content/themes/aulavirtual/page.php
+++ aula/wp-content/themes/aulavirtual/page.php
@@ -101,22 +101,22 @@
         </div>
       </div>
       <div class="row" id="contenedor-aula-musica">
-        <div class="col-lg-offset-5 col-lg-6 col-md-offset-3 col-md-8" id="div-info-andina">
+        <div class="col-lg-offset-4 col-lg-7 col-md-offset-3 col-md-8" id="div-info-andina">
           <div class="col-lg-offset-1 col-lg-11">
             <h2>La Música Andina</h2>
             <br>
             <p>Es un término que se aplica a una gama muy vasta de géneros musicales originados en los Andes sudamericanos. Esta área incluye principalmente los andes del Perú y Bolivia; sierras de Ecuador, noroeste de Argentina, norte de Chile y suroeste de Colombia y Venezuela.<br><br>El término se usa a menudo como sinónimo del estilo musical típico del altiplano e interpretado generalmente por aymaras, quechuas y otros pueblos de dicha región, estilo caracterizado por melodías nostálgicas y evocativas interpretadas con flautas de caña y charangos. Pero en sentido estricto la expresión "música andina" englobaría no sólo esta música sino también los restantes estilos y formaciones instrumentales presentes a lo largo y ancho de la geografía andina</p>
           </div>
-          <div class="col-lg-4 col-md-4 text-center">
-            <a href="#" id="btn-sala-teorica"><img src="<?php bloginfo('template_url'); ?>/musica/images/icono_sala_teorica.png">
+          <div class="col-lg-offset-2 col-lg-3 col-md-4 text-center">
+            <a href="#" id="btn-sala-teorica"><img src="<?php bloginfo('template_url'); ?>/musica/images/icono_sala_teorica.png" class="img-responsive" style="width: 150px;">
               <button class="btn btn-block btn-musica-s">Sala Teórica</button></a>
             </div>
-            <div class="col-lg-4 col-md-4 text-center">
-              <a href="#" id="btn-sala-interactiva"><img src="<?php bloginfo('template_url'); ?>/musica/images/icono_sala_interactiva.png">
+            <div class="col-lg-3 col-md-4 text-center">
+              <a href="#" id="btn-sala-interactiva"><img src="<?php bloginfo('template_url'); ?>/musica/images/icono_sala_interactiva.png" class="img-responsive" style="width: 150px;">
                 <button class="btn btn-block btn-musica-s">Sala Interactiva</button></a>
               </div>
-              <div class="col-lg-4 col-md-4 text-center">
-                <a href="#" id="btn-sala-general"><img src="<?php bloginfo('template_url'); ?>/musica/images/icono_sala_general.png">
+              <div class="col-lg-3 col-md-4 text-center">
+                <a href="#" id="btn-sala-general"><img src="<?php bloginfo('template_url'); ?>/musica/images/icono_sala_general.png" class="img-responsive" style="width: 150px;">
                   <button class="btn btn-block btn-musica-s">Sala General</button></a>
                 </div>
               </div>
